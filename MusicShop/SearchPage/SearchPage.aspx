1c66d3ebbd3fd120631c614c22576286b45c6c3f
ParkerKalthoff <parkerlkalthoff@gmail.com>
2023-12-06 21:33:42 -0600

    update

--- MusicShop/SearchPage/SearchPage.aspx
+++ MusicShop/SearchPage/SearchPage.aspx
@@ -6,12 +6,15 @@
     <div style="position: fixed; width: 80%; height: 60%; background-color: rgba(255, 255, 255, 0.8); margin-left: 10%; margin-top: 5%;">
         <div style="margin-left: 5%; margin-top: 10%; height: auto; width: auto;">
             <div class="dropdown">
-                <asp:DropDownList ID="DropDownList1" runat="server"></asp:DropDownList>
+                <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged"></asp:DropDownList>
+                <asp:Button runat="server" Text="Select Genre" OnClick="Unnamed1_Click"></asp:Button>
+                
             </div>
+            <div class="dropdown">&nbsp;<asp:Label runat="server" Text="Label"></asp:Label></div>
         </div>
         <div style="margin-left: 5%; margin-top: 10%; height: auto; width: auto;">
             <div class="dropdown">
-                <asp:DropDownList ID="DropDownList2" runat="server"></asp:DropDownList>
+                <asp:DropDownList ID="DropDownList2" runat="server" Visible="True"></asp:DropDownList>
             </div>
         </div>
     </div>
--- MusicShop/SearchPage/SearchPage.aspx.cs
+++ MusicShop/SearchPage/SearchPage.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,13 +10,46 @@ namespace MusicShop.SearchPage
 {
     public partial class SearchPage : System.Web.UI.Page
     {
+        MusicShopDataContext dbcon;
+        string connString = MyStaticValues.conn;
+        int test = 1;
+        
         protected void Page_Load(object sender, EventArgs e)
         {
             // orignal connString declared in global.asax.cs
-            string connString = MyStaticValues.conn;
+            
+
+
+            using (dbcon = new MusicShopDataContext(connString))
+            {
+                // Correlated Ids and Names have same array position
+                MyStaticValues.genreIds = dbcon.Genres.Select(ids => ids.GenreID).ToArray();
+                MyStaticValues.genreNames = dbcon.Genres.Select(names => names.GenreName).ToArray();
+
+                DropDownList1.DataSource = MyStaticValues.genreNames;
+                DropDownList1.DataBind();
+            }
+        }
 
 
 
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            using (dbcon = new MusicShopDataContext(connString))
+            {
+                int selectedID = MyStaticValues.genreIds[DropDownList1.SelectedIndex];
+
+                MyStaticValues.artistNames = dbcon.Artists.Where(artist => artist.GenreID == selectedID).Select(artist => artist.ArtistName).ToArray();
+
+                DropDownList1.SelectedIndex = DropDownList1.SelectedIndex;
+                DropDownList2.DataSource = MyStaticValues.artistNames;
+                DropDownList2.DataBind();
+            }
+        }
+
+        protected void Unnamed1_Click(object sender, EventArgs e)
+        {
         }
     }
 }
